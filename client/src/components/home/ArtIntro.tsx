b902cf025a8be70450287a9ed2a7bda33ccf3a20
izzibizz <contact@izabellind.com>
2026-05-07 10:22:53 +0200

    dration anim

--- client/src/components/home/ArtIntro.tsx
+++ client/src/components/home/ArtIntro.tsx
@@ -225,7 +225,7 @@ export function ArtIntro({
                 initial={{ opacity: 0, y: 80 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{
-                  duration: 3,
+                  duration: 2,
                   ease: [0.22, 1, 0.36, 1],
                 }}
                 viewport={{ once: true, amount: 0.2 }}
